611d573046f46520752ed01eaef2be2669f33398
goomin <rkdalsrna@gachon.ac.kr>
2026-03-20 18:22:07 +0900

    week1 update

--- goomin/week1/mingooApp/app/src/main/java/com/example/mingooapp/MainActivity.kt
+++ goomin/week1/mingooApp/app/src/main/java/com/example/mingooapp/MainActivity.kt
@@ -21,21 +21,42 @@ class MainActivity : AppCompatActivity() {
         //위에서 정의한 버튼을 클릭했을 때의 행동 정의
         binding.imageView1.setOnClickListener {
             binding.textView4.setTextColor(Color.YELLOW)
+            binding.textView5.setTextColor(Color.BLACK)
+            binding.textView6.setTextColor(Color.BLACK)
+            binding.textView7.setTextColor(Color.BLACK)
+            binding.textView8.setTextColor(Color.BLACK)
         }
 
         binding.imageView2.setOnClickListener {
+            binding.textView4.setTextColor(Color.BLACK)
             binding.textView5.setTextColor(Color.CYAN)
+            binding.textView6.setTextColor(Color.BLACK)
+            binding.textView7.setTextColor(Color.BLACK)
+            binding.textView8.setTextColor(Color.BLACK)
         }
 
         binding.imageView3.setOnClickListener {
+            binding.textView4.setTextColor(Color.BLACK)
+            binding.textView5.setTextColor(Color.BLACK)
             binding.textView6.setTextColor(Color.parseColor("#800080"))
+            binding.textView7.setTextColor(Color.BLACK)
+            binding.textView8.setTextColor(Color.BLACK)
         }
 
         binding.imageView4.setOnClickListener {
+            binding.textView4.setTextColor(Color.BLACK)
+            binding.textView5.setTextColor(Color.BLACK)
+            binding.textView6.setTextColor(Color.BLACK)
             binding.textView7.setTextColor(Color.GREEN)
+            binding.textView8.setTextColor(Color.BLACK)
+
         }
 
         binding.imageView5.setOnClickListener {
+            binding.textView4.setTextColor(Color.BLACK)
+            binding.textView5.setTextColor(Color.BLACK)
+            binding.textView6.setTextColor(Color.BLACK)
+            binding.textView7.setTextColor(Color.BLACK)
             binding.textView8.setTextColor(Color.RED)
         }
 
