00c8b2262bd1add1b804f4499767873910a75337
shinhanseo <imkara1@naver.com>
2026-03-30 16:21:09 +0900

    3주차 과제 업로드

---
+++ seoya/week3/app/src/main/java/com/example/myapplication/HomeData.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+class HomeData (
+    val imageResId: Int,
+    val name: String,
+    val price: String
+)
+
---
+++ seoya/week3/app/src/main/java/com/example/myapplication/WishlistData.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication
+
+class WishlistData(
+    val imageResId: Int,
+    val name: String,
+    val subTitle: String,
+    val colorText: String,
+    val price: String
+)
